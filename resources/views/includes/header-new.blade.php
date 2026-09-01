d4b0552f2a8166fdb14f4cd265d2daf8bf3e2584
Atif Malik <atifmalik2009@gmail.com>
2026-09-01 11:09:27 +0500

    changes for no index

--- resources/views/includes/header-new.blade.php
+++ resources/views/includes/header-new.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="robots" content="noindex, nofollow">
     <meta  name="description" content="%DESCRIPTION%">
 	<meta  name="keywords" content="%KEYWORD% ">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,7 +4,6 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="robots" content="noindex, nofollow">
 	<meta name="google-site-verification" content="JgeA_wkUnETUFTNjhTg3ttly5YbcmzP2tSo8S3Y5ebA" />
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 	<meta name="csrf-token" content="{{ csrf_token() }}">
